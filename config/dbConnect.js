0ccd55388b44e4bed3684b3a17e1f5e391d4fff7
Abhay <yabhay@iiitmanipur.ac.in>
2026-05-15 18:31:53 +0530

    changed db to test [vercel skip]

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,30 +1,30 @@
 const mongoose = require('mongoose')
 
-const dbConnect = ()=>{
-    try{
-        const connection = mongoose.connect(process.env.MONGODB_URL,{
-            dbName:"geekOnDemand",
-            authSource: "admin",
-        })
-        // console.log(process.env.MONGODB_URL)
-        connection.then(()=>{console.log("✅ Connected to DB");})
-    }catch(e){
-        console.error("❌ Database connection error:", e.message); 
-    }
-}
-
-
-// const dbConnect = ()=>{ 
+// const dbConnect = ()=>{
 //     try{
-//         const connection = mongoose.connect(process.env.MONGODB_TEST_URL,{
+//         const connection = mongoose.connect(process.env.MONGODB_URL,{
 //             dbName:"geekOnDemand",
 //             authSource: "admin",
 //         })
 //         // console.log(process.env.MONGODB_URL)
-//         connection.then(()=>{console.log("✅ Connected to test DB");})
+//         connection.then(()=>{console.log("✅ Connected to DB");})
 //     }catch(e){
 //         console.error("❌ Database connection error:", e.message); 
 //     }
 // }
+
+
+const dbConnect = ()=>{ 
+    try{
+        const connection = mongoose.connect(process.env.MONGODB_TEST_URL,{
+            dbName:"geekOnDemand",
+            authSource: "admin",
+        })
+        // console.log(process.env.MONGODB_URL)
+        connection.then(()=>{console.log("✅ Connected to test DB");})
+    }catch(e){
+        console.error("❌ Database connection error:", e.message); 
+    }
+}
 
 module.exports = dbConnect;
